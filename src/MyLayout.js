558f55d705422780d93629f8d009e5f43ea707f9
MatthewLoffredo <mattloffredo808@gmail.com>
2019-11-12 00:24:46 -0600

    changed files

---
+++ src/MyLayout.js
@@ -0,0 +1,161 @@
+import React, {useEffect, useState} from 'react';
+import {
+    Button,
+    Col,
+    Collapse,
+    Container,
+    DropdownItem,
+    DropdownMenu,
+    DropdownToggle,
+    Form,
+    Input,
+    InputGroup,
+    InputGroupAddon,
+    ListGroup,
+    ListGroupItem,
+    Nav,
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    NavItem,
+    NavLink,
+    Row,
+    UncontrolledDropdown
+} from "reactstrap";
+import fetch from "isomorphic-unfetch";
+
+const Layout = (props) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const [searchQuery, setQuery] = useState('');
+    const [leftMenu, setLeftMenu] = useState(false);
+    const [page, setPage] = useState(1);
+
+
+    const toggle = () => setIsOpen(!isOpen);
+    const changeSearch = (event) => {
+        setQuery(event.target.value);
+    };
+
+    const search = (event) => {
+        if (searchQuery.length > 0)
+            searchWithoutEvent()
+        event.preventDefault();
+    };
+
+    const searchWithoutEvent = () => {
+        fetch('http://clearancegood-env.xe4i3r2rmx.us-east-2.elasticbeanstalk.com/goods?q=' + searchQuery).then(function (response) {
+            response.json().then(data => {
+                props.handleSearch(data);
+                setPage(1);
+            });
+
+        });
+    }
+
+    const handleLeftMenu = (event) => {
+        const q = event.currentTarget.textContent;
+        setLeftMenu(true);
+        setQuery(q);
+    }
+
+    useEffect(() => {
+        if (leftMenu) {
+            setLeftMenu(false);
+            searchWithoutEvent();
+        }
+    }, [searchQuery]);
+
+    const nextPage = (event) => {
+        let url = 'http://clearancegood-env.xe4i3r2rmx.us-east-2.elasticbeanstalk.com/goods?page=' + (page + 1);
+        if (searchQuery.length > 0) url += "&q=" + searchQuery;
+        fetch(url).then(function (response) {
+            response.json().then(data => {
+                props.handleNextPage(data);
+                setPage(page + 1);
+            });
+        });
+    };
+    return (
+        <div>
+            <Navbar color="dark" dark expand="md">
+                <NavbarBrand href="/">Price Hunter</NavbarBrand>
+                <NavbarToggler onClick={toggle}/>
+                <Collapse isOpen={isOpen} navbar>
+                    <Nav className="ml-auto" navbar>
+                        <NavItem>
+                            <NavLink href="/components/">Components</NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/uuganbold/clearance-goods">GitHub</NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                            <DropdownToggle nav caret>
+                                Options
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    Option 1
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Option 2
+                                </DropdownItem>
+                                <DropdownItem divider/>
+                                <DropdownItem>
+                                    Reset
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                </Collapse>
+            </Navbar>
+            <Container>
+                <Row>
+                    <Col lg="3">
+                        <h1 className="my-4">Shop Name</h1>
+                        <ListGroup>
+                            <ListGroupItem onClick={handleLeftMenu} data-query={'Halloween'}>Halloween</ListGroupItem>
+                            <ListGroupItem onClick={handleLeftMenu} data-query={'Shoes'}>Shoes</ListGroupItem>
+                            <ListGroupItem onClick={handleLeftMenu} data-query={'Women'}>Women</ListGroupItem>
+                            <ListGroupItem onClick={handleLeftMenu} data-query={'Men'}>Men</ListGroupItem>
+                            <ListGroupItem onClick={handleLeftMenu} data-query={'Kids'}>Kids</ListGroupItem>
+                            <ListGroupItem onClick={handleLeftMenu} data-query={'Clothing'}>Clothing</ListGroupItem>
+                            <ListGroupItem onClick={handleLeftMenu} data-query={'Beauty'}>Beauty</ListGroupItem>
+
+                        </ListGroup>
+                    </Col>
+                    <Col lg="9">
+                        <Container>
+                            <Row className="py-2 my-2">
+                                <Col lg={12} md={6} sm={4}>
+                                    <Form onSubmit={search}>
+                                        <InputGroup>
+                                            <Input value={searchQuery} onChange={changeSearch}/>
+                                            <InputGroupAddon
+                                                addonType="prepend"><Button>Search</Button></InputGroupAddon>
+                                        </InputGroup>
+                                    </Form>
+
+                                </Col>
+                            </Row>
+                        </Container>
+                        {props.children}
+                        <Row className="my-2">
+                            <Col lg={12} style={{textAlign: 'center'}}>
+                                <Button style={{width: 500}} color="light" onClick={nextPage}>Next Page</Button>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+
+
+            <footer className="py-5 bg-dark">
+                <div className="container">
+                    <p className="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+                </div>
+            </footer>
+        </div>
+    );
+};
+
+export default Layout;
